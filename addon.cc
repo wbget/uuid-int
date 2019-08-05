3d0967854b7ab5160ef4b0435c144f4c9614b31e
Webb <xwb1990@gmail.com>
2019-08-05 22:50:58 +0800

    fix win error

--- addon.cc
+++ addon.cc
@@ -1,45 +1,32 @@
-#include <napi.h>
-#include "generator.h"
-#include <ctime>
+#include "myobject.h"
+#include <assert.h>
 
-Napi::Object CreateGenerator(const Napi::CallbackInfo &info)
+napi_value CreateObject(napi_env env, napi_callback_info info)
 {
-    Napi::Env env = info.Env();
-    if (info.Length() < 1)
-    {
-        Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
-        return Napi::Object::New(env);
-    }
-    if (!info[0].IsNumber())
-    {
-        Napi::TypeError::New(env, "id must be number").ThrowAsJavaScriptException();
-        return Napi::Object::New(env);
-    }
-    Napi::Number par0 = info[0].As<Napi::Number>();
-    int id = par0.Int32Value();
-    if (id < 0 || id > 511)
-    {
-        Napi::RangeError::New(env, "id must be >=0 or <= 511").ThrowAsJavaScriptException();
-        return Napi::Object::New(env);
-    }
-    if (!info[1].IsNumber())
-    {
-        Napi::TypeError::New(env, "seed must be number").ThrowAsJavaScriptException();
-        return Napi::Object::New(env);
-    }
-    int par1 = info[1].As<Napi::Number>().Int32Value();
-    if (par1 < 0 || par1 > time(0))
-    {
-        Napi::RangeError::New(env, "seed must <= now").ThrowAsJavaScriptException();
-        return Napi::Object::New(env);
-    }
-    return Generator::NewInstance(env, info[0], info[1]);
+    napi_status status;
+
+    size_t argc = 2;
+    napi_value args[2];
+    status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    assert(status == napi_ok);
+
+    napi_value instance;
+    status = MyObject::NewInstance(env, args[0], args[1], &instance);
+    assert(status == napi_ok);
+
+    return instance;
 }
 
-Napi::Object InitAll(Napi::Env env, Napi::Object exports)
+napi_value Init(napi_env env, napi_value exports)
 {
-    Napi::Object new_exports = Napi::Function::New(env, CreateGenerator, "CreateGenerator");
-    return Generator::Init(env, new_exports);
+    napi_status status = MyObject::Init(env);
+    assert(status == napi_ok);
+
+    napi_value new_exports;
+    status =
+        napi_create_function(env, "", NAPI_AUTO_LENGTH, CreateObject, nullptr, &new_exports);
+    assert(status == napi_ok);
+    return new_exports;
 }
 
-NODE_API_MODULE(addon, InitAll)
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
--- binding.gyp
+++ binding.gyp
@@ -3,14 +3,7 @@
     "targets": [
         {
             "target_name": "addon",
-            "cflags!": ["-fno-exceptions"],
-            "cflags_cc!": ["-fno-exceptions"],
-            "sources": ["addon.cc", 'generator.cc'],
-            "include_dirs": [
-                "<!@(node -p \"require('node-addon-api').include\")"
-            ],
-            'dependencies': ["<!(node -p \"require('node-addon-api').gyp\")"],
-            'defines': ['NAPI_DISABLE_CPP_EXCEPTIONS'],
-        }
+            "sources": ["addon.cc", 'myobject.cc']
+        },
     ]
 }
